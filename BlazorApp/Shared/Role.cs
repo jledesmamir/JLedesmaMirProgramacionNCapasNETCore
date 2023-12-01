ae17e109ee45bd7aba4ddbee83eac348f32a4d7c
jledesmamir <132494971+jledesmamir@users.noreply.github.com>
2023-12-01 11:55:14 -0600

    Delete BlazorApp directory
    Eliminación de archivo.

--- BlazorApp/Shared/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorApp.Shared
-{
-    public class Role
-    {
-        public Guid IdRol { get; set; }
-        public string? Name { get; set; }
-        public List<object>? Roles { get; set; }
-    }
-}
